19ad5e2ebd3d14482dbe0c97a83aa026758594e9
Yvelchrome <yvelchrome@gmail.com>
2022-11-15 16:00:14 +0100

    feat: manager

---
+++ app/src/Manager/UserManager.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Manager;
+
+use App\Entity\User;
+
+class UserManager extends BaseManager
+{
+
+    /**
+     * @return User[]
+     */
+    public function getAllUsers(): array
+    {
+        $query = $this->pdo->query("select * from User");
+
+        $users = [];
+
+        while ($data = $query->fetch(\PDO::FETCH_ASSOC)) {
+            $users[] = new User($data);
+        }
+
+        return $users;
+    }
+}
