14fab3f525b53a14ffc528bd932e9d324e942148
Snwolfy <97434478+Snwolfy@users.noreply.github.com>
2022-11-15 16:19:10 +0100

    feat : pdo factory

---
+++ app/src/Factory/PDOFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Factory;
+
+use App\Interfaces\Database;
+use PDO;
+
+class PDOFactory implements Database
+{
+    private string $driver;
+    private string $host;
+    private int $port;
+    private string $dbName;
+    private string $user;
+    private string $assword;
+
+    public function __construct(string $driver = "mysql", string $host = "database", int $port = 3306, string $dbName = "data", string $user = "root", string $password = "password")
+    {
+        $this->driver = $driver;
+        $this->host = $host;
+        $this->port = $port;
+        $this->dbName = $dbName;
+        $this->user = $user;
+        $this->password = $password;
+    }
+
+    public function getMySqlPDO(): \PDO
+    {
+        return new \PDO($this->driver . ":host" . $this->host . ":" . $this->port . ";dbname=" . $this->dbName, $this->user, $this->password);
+    }
+}
