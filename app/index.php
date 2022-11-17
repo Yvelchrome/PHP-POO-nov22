1f450af7c0d899b35a9172dc9f1090e4c52198b5
Yvelchrome <yvelchrome@gmail.com>
2022-11-17 13:40:38 +0100

    feat: head and footer

--- app/index.php
+++ app/index.php
@@ -6,6 +6,13 @@ $url = "/" . trim(explode("?", $_SERVER['REQUEST_URI'])[0], "/");
 
 switch ($url) {
     case "/":
+        require_once __DIR__ . "/Views/head.php";
+        require_once __DIR__ . "/Views/SignUp.php";
+        require_once __DIR__ . "/Views/footer.php";
+        break;
+    case "/login":
+        require_once __DIR__ . "/Views/head.php";
         require_once __DIR__ . "/Views/SignIn.php";
+        require_once __DIR__ . "/Views/footer.php";
         break;
 }
