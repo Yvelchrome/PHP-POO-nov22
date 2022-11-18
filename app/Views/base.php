d706bbdca4f099efac68b1dc0ce1a9a55851fbfd
Yvelchrome <yvelchrome@gmail.com>
2022-11-18 15:47:14 +0100

    refactor: css tree organization and added scss

--- app/Views/base.php
+++ app/Views/base.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel=icon href="../src/assets/icons/favicon.svg">
-    <link rel="stylesheet" href="../src/assets/css/style.css">
+    <link rel="stylesheet" href="../src/assets/css/dist/style.css">
     <title><?= $_pageTitle; ?></title>
 </head>
 
