f0bd0d29fcfebc7565590ad0499b490f545a56a3
Snwolfy <97434478+Snwolfy@users.noreply.github.com>
2022-11-24 11:05:29 +0100

    refactor: changing addUser and checkUser to object

--- app/src/Controller/RegisterController.php
+++ app/src/Controller/RegisterController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Manager\UserManager;
 use App\Factory\PDOFactory;
 use App\Route\Route;
@@ -15,13 +16,18 @@ class RegisterController extends AbstractController
     }
     #[Route("/", name: "register", methods: ["POST"])]
     public function executeAdd()
-    {
+    {   
+        $newUser = new User();
         $username = $_POST["username"];
         $email = $_POST["email"];
         $password = hash("sha512", $_POST["password"]);
         $admin = $_POST["admin"];
+        $newUser->setUsername($username);
+        $newUser->setEmail($email);
+        $newUser->setPassword($password);
+        $newUser->setAdmin($admin);
         $manager = new UserManager(new PDOFactory());
-        $manager->addUser($username, $email, $password, $admin);
+        $manager->addUser($newUser);
 
         header("Location: /home");
     }
--- app/src/Manager/UserManager.php
+++ app/src/Manager/UserManager.php
@@ -24,35 +24,36 @@ class UserManager extends BaseManager
         return $users;
     }
 
-    public function addUser(string $username, string $email, string $password, int $admin)
+    public function addUser(User $user): ?User
     {;
         session_start();
         $insert = $this->pdo->prepare("INSERT INTO User (username, email, password, admin) VALUES(:username, :email, :password, :admin)");
-        $insert->bindValue("username", $username, PDO::PARAM_STR);
-        $insert->bindValue("email", $email, PDO::PARAM_STR);
-        $insert->bindValue("password", $password, PDO::PARAM_STR);
-        $insert->bindValue("admin", $admin, PDO::PARAM_INT);
+        $insert->bindValue("username", $user->getUsername(), PDO::PARAM_STR);
+        $insert->bindValue("email", $user->getEmail(), PDO::PARAM_STR);
+        $insert->bindValue("password", $user->getPassword(), PDO::PARAM_STR);
+        $insert->bindValue("admin", $user->getAdmin(), PDO::PARAM_INT);
         $insert->execute();
 
         $users = $this->pdo->prepare("SELECT * FROM User WHERE username = :username");
-        $users->bindValue("username", $username, PDO::PARAM_STR);
+        $users->bindValue("username", $user->getUsername(), PDO::PARAM_STR);
         $users->execute();
         $userFetch = $users->fetch(PDO::FETCH_ASSOC);
         $_SESSION["User"] = $userFetch;
+        return $user;
     }
 
-    public function checkUser(string $username, string $password): ?User
+    public function checkUser(User $user): ?User
     {
         $checking = $this->pdo->prepare("SELECT * FROM User WHERE username = :username and password = :password");
 
-        $checking->bindValue("username", $username, PDO::PARAM_STR);
-        $checking->bindValue("password", $password, PDO::PARAM_STR);
+        $checking->bindValue("username", $user->getUsername(), PDO::PARAM_STR);
+        $checking->bindValue("password", $user->getPassword(), PDO::PARAM_STR);
         $checking->execute();
         $userFetch = $checking->fetch(PDO::FETCH_ASSOC);
         $_SESSION["User"] = $userFetch;
 
         if ($userFetch) {
-            return new User($userFetch);
+            return $user;
         }
         return null;
     }
