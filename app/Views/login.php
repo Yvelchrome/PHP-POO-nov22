b20469967e862534107d773f7c86decb166a2852
Yvelchrome <yvelchrome@gmail.com>
2022-11-27 23:50:08 +0100

    fix: removed regex on login page due to pre-created users

--- app/Views/login.php
+++ app/Views/login.php
@@ -12,11 +12,11 @@
         <form class="container__form" method="post">
             <label class=" container__form__label">
                 Username
-                <input class="container__form__input" name="username" type="text" pattern="^[a-z0-9_-]{3,15}$" required>
+                <input class="container__form__input" name="username" type="text" required>
             </label>
             <label class="container__form__label">
                 Password
-                <input class="container__form__input" name="password" type="password" pattern="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$" required>
+                <input class="container__form__input" name="password" type="password" required>
             </label>
             <input class="container__form__submit" name="connection" type="submit" value="SIGN IN">
         </form>
