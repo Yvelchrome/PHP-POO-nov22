ff7946fc82174fa534963ec79492a1ebab185bde
Yvelchrome <yvelchrome@gmail.com>
2022-11-27 23:09:51 +0100

    fix: hide header user list link if not admin

--- app/Views/header.php
+++ app/Views/header.php
@@ -3,7 +3,9 @@
     <ul class="list">
         <li><a class="list-link" href="/home">Home</a></li>
         <li><a class="list-link" href="/admin">Admin</a></li>
-        <li><a class="list-link" href="<?= $_SESSION["User"]["admin"] == 1 ? "/admin/userList" : "javascript:void(0)" ?>">User List</a></li>
+        <?php if ($_SESSION["User"]["admin"] == 1) : ?>
+            <li><a class="list-link" href="/admin/userList">User List</a></li>
+        <?php endif ?>
         <li><a class="list-link" href="/login">Logout</a></li>
     </ul>
 </header>
