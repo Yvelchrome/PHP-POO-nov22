76e5c54dc61c03228b4800d9b87dc18dbfb990af
Yvelchrome <yvelchrome@gmail.com>
2022-11-27 23:21:56 +0100

    fix: head src path fix

--- app/Views/base.php
+++ app/Views/base.php
@@ -5,8 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel=icon href="../src/assets/images/icons/favicon.svg">
-    <link rel="stylesheet" href="../src/assets/css/dist/style.css">
+    <link rel=icon href="/src/assets/images/icons/favicon.svg">
+    <link rel="stylesheet" href="/src/assets/css/dist/style.css">
     <title><?= $_pageTitle; ?></title>
 </head>
 
